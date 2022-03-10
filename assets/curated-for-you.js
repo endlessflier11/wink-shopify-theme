7d422e08eff2b7778a9c2c320f377b9b544e9e72
endlessflier11 <endlessflier@yahoo.com>
2022-03-10 11:10:41 +0800

    first commit

---
+++ assets/curated-for-you.js
@@ -0,0 +1,9 @@
+// (function () {
+//   var iframe = document.getElementById('iFrameCurated');
+//   iframe.addEventListener('load', function(event) {
+//     console.log("222=", iframe);
+//     var iFrameHeight = this.contentWindow.document.documentElement.scrollHeight + 'px';
+//     console.log("666=", iFrameHeight);
+//     //iframe.style.height
+//   });
+// })();
